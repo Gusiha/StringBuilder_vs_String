45682bcc56cd3987ecb0f486b60bdf3169f53738
TUGARIN <TUGARIN@TUGARIN>
2022-10-12 22:04:16 +0300

    Add project files.

---
+++ StringBuilder_vs_String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+string greeting = "Hello World";
+StringBuilder stringBuilder = new StringBuilder("Hello World");
+
+
+DateTime start = DateTime.Now;
+for (long i = 0; i < 100000; i++)
+{
+    greeting += "HelloAgain";
+}
+DateTime end = DateTime.Now;
+Console.WriteLine($"String Concat : {end - start}");
+
+
+start = DateTime.Now;
+for (long i = 0; i < 100000; i++)
+{
+    stringBuilder.Append("HelloAgain");
+}
+string Copy = stringBuilder.ToString();
+end = DateTime.Now;
+
+Console.WriteLine($"StringBuilder Concat : {end - start}");
